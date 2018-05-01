96c444216d314787a69dca6e621fbf7d538cb82d
Dana Shehab <danashehab@Danas-MacBook-Pro.local>
2018-05-01 17:48:37 +0300

    Backend

--- books/models.py
+++ books/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Book(models.Model):
-	book_name   = models.ChatField()
+	book_name   = models.CharField()
 	cover_image = models.ImageField(null=True)
 	book_description = models.TextField(max_length=300, null=True)
 	
