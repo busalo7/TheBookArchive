69f9da2bc1b22ab6abc3de217175921f6dadcc59
Dana Shehab <danashehab@Danas-MacBook-Pro.local>
2018-05-02 19:42:42 +0300

    requiremts file

--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework_jwt.settings import api_settings
-from books.models import Book, Page
+from books.models import Book, Page, Profile
 from django.http import JsonResponse 
 
 
@@ -36,4 +36,24 @@ class PageCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Page
         fields = '__all__'
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(style={'input_type': 'password'}, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+    def create(self, validated_data):
+        username = validated_data['username']
+        password = validated_data['password']
+        new_user = User(username=username)
+        new_user.set_password(password)
+        new_user.save()
+
+        new_profile = Profile(user=new_user)
+        new_profile.save()
+
+        return validated_data
+
 
--- books/models.py
+++ books/models.py
@@ -2,6 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+	user = models.OneToOneField(User , on_delete=models.CASCADE)
+	profile_image = models.ImageField()
+	profile_bio = models.TextField(max_length=300, null=True)
+
+
+
 class Book(models.Model):
 	book_name   = models.CharField(max_length=20)
 	cover_image = models.ImageField(null=True)
